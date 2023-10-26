81bae7c044e4bc807c09e4c5f4be3603d57dd28b
marylizkarumba <marylizkarumba791@gmail.com>
2023-10-26 11:57:12 +0300

    Delete 0x02-react_props directory

--- 0x02-react_props/task_0/dashboard/src/App/App.test.js
+++
@@ -1,9 +0,0 @@
-import { shallow } from 'enzyme';
-import App from './App';
-
-describe('Test App.js', () => {
-  it('App without crashing', (done) => {
-    expect(shallow(<App />).exists());
-    done();
-  });
-});
